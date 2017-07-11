a9d528ef5a04046e4777ee06e578d2852ed0bde0
xiecheng <13136666328@163.com>
2017-07-11 17:02:02 +0800

    美团

--- meituanci/application/views/login.php
+++ meituanci/application/views/login.php
@@ -7,23 +7,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <base href="<?php echo site_url(); ?>">
     <link rel="shortcut icon" href="img/favicon.ico"/>
+    <link rel="stylesheet" href="css/jquery.mobile-1.4.5.css">
     <link rel="stylesheet" href="css/common.css">
     <link rel="stylesheet" href="css/login.css">
     <script src="javascript/rem.js"></script>
+    <script src="javascript/jquery-1.12.4.js"></script>
+    <script src="javascript/index.js"></script>
+    <script src="javascript/jquery.mobile-1.4.5.js"></script>
     <title>Document</title>
 </head>
 <body>
-<form action="welcome/login" id="login-form" method="post">
-    <div>
-        <label for="username">用户名:</label>
-        <input type="text" name="username" id="username" placeholder="请输入用户名">
-        <label for="password">密码:</label>
-        <input type="password" name="password" id="password" placeholder="请输入密码">
-        <input type="submit" value="登录">
+    <div id="header">
+        登录
     </div>
-
-</form>
-
-
+    <form method="post" action="welcome/login" data-ajax="false" id="login-form">
+        <div class="ui-field-contain">
+            <label for="username">用户名:</label>
+            <input type="text" name="username" id="username" placeholder="请输入用户名">
+            <label for="password">密码:</label>
+            <input type="password" name="password" id="password" placeholder="请输入密码">
+            <button class="ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-icon-left ui-icon-user">登录</button>
+            <button class="ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-icon-left ui-icon-tag" type="button">注册</button>
+        </div>
+    </form>
 </body>
 </html>
