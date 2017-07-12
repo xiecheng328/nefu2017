de76cbf189ff60751fd3a94595fcf77ca0f6f9ea
xiecheng <13136666328@163.com>
2017-07-12 15:04:57 +0800

    美团登录注册

--- meituanci/application/controllers/User.php
+++ meituanci/application/controllers/User.php
@@ -2,12 +2,40 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        $this -> load -> model("user_model");
+    }
 
+    public function login_page()
+    {
+        $this -> load -> view('login');
+    }
+
+    public function login()
+    {
+        $username = $this -> input -> post("username");
+        $password = $this -> input -> post("password");
+
+        $row = $this -> user_model -> get_by_username_password($username, $password);
+        if($row){
+            echo "登录成功";
+        } else {
+            echo "登录失败";
+        }
+    }
+
+    public function register_page()
+    {
+        $this->load->helper(array('form', 'url'));
+        $this->load->library('form_validation');
+        $this -> load -> view("register");
+    }
 
 	public function check_username()
     {
         $username = $this -> input -> post("username");
-        $this -> load -> model("user_model");
         $row = $this -> user_model -> check_username($username);
         if($row){
             echo "no";
@@ -20,10 +48,9 @@ class User extends CI_Controller {
     {
         $username = $this -> input -> post("username");
         $password = $this -> input -> post("password");
-        $this -> load -> model("user_model");
         $num = $this -> user_model -> insert_user($username, $password);
         if($num > 0){//插入成功
-            redirect("welcome/login_page");
+            redirect("user/login_page");
         }
     }
 
