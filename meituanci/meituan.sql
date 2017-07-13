e81c3c05671ac8a304d3d2a2a2034c39dfca154d
xiecheng <13136666328@163.com>
2017-07-13 15:48:54 +0800

    美团数据库脚本

--- meituanci/meituan.sql
+++ meituanci/meituan.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100124
  File Encoding         : utf-8
 
- Date: 07/13/2017 10:33:13 AM
+ Date: 07/13/2017 15:47:56 PM
 */
 
 SET FOREIGN_KEY_CHECKS = 0;
@@ -24,19 +24,20 @@ CREATE TABLE `t_order` (
   `order_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL,
-  `order_price` decimal(10,0) DEFAULT NULL,
+  `order_price` varchar(10) COLLATE utf8_bin DEFAULT NULL,
+  `num` int(11) DEFAULT NULL COMMENT '数量',
   PRIMARY KEY (`order_id`),
   KEY `user_id` (`user_id`),
   KEY `product_id` (`product_id`),
   CONSTRAINT `t_order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`user_id`),
   CONSTRAINT `t_order_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `t_product` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 --  Records of `t_order`
 -- ----------------------------
 BEGIN;
-INSERT INTO `t_order` VALUES ('1', '1', '1', '5'), ('2', '1', '2', '10');
+INSERT INTO `t_order` VALUES ('1', '1', '1', '5', '2'), ('2', '1', '2', '10', '3'), ('3', '1', '3', '8', '2');
 COMMIT;
 
 -- ----------------------------
@@ -46,15 +47,15 @@ DROP TABLE IF EXISTS `t_product`;
 CREATE TABLE `t_product` (
   `product_id` int(11) NOT NULL AUTO_INCREMENT,
   `product_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `price` decimal(10,0) DEFAULT NULL,
+  `price` varchar(10) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 --  Records of `t_product`
 -- ----------------------------
 BEGIN;
-INSERT INTO `t_product` VALUES ('1', '鲜芋仙', '8'), ('2', '肯德基', '15');
+INSERT INTO `t_product` VALUES ('1', 'xianyuxian', '8'), ('2', 'KFC', '15'), ('3', '麦当劳', '2');
 COMMIT;
 
 -- ----------------------------
@@ -66,13 +67,13 @@ CREATE TABLE `t_user` (
   `username` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '用户名称',
   `password` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '密码',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 --  Records of `t_user`
 -- ----------------------------
 BEGIN;
-INSERT INTO `t_user` VALUES ('1', 'admin', '12345'), ('2', 'aa', 'aa'), ('8', 'abc', 'abc'), ('9', 'zz', 'zz'), ('11', 'xx', 'xx'), ('12', 'cc', 'cc');
+INSERT INTO `t_user` VALUES ('1', 'admin', '12345'), ('2', 'aa', 'aa'), ('8', 'abc', 'abc'), ('9', 'zz', 'zz'), ('11', 'xx', 'xx'), ('12', 'cc', 'cc'), ('13', 'nn', 'nn');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
